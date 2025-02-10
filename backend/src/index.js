add17339bc614eaaa19df59e531b88d6ab28089f
venkatesh <venkatesh1754673niz@gmail.com>
2025-02-10 16:29:16 +0530

    modified routes for auth and messages

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,20 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import {connectDB} from "./lib/db.js";
 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 const app=express();
 dotenv.config();
 const PORT= process.env.PORT;
-app.use(express.json());
 app.use(cookieParser());
+app.use(express.json());
+
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 
 app.listen(PORT,()=>{
