1ea70b801d19f1297f1cc56524fbe7e3b401d702
venkatesh <venkatesh1754673niz@gmail.com>
2025-02-12 09:44:39 +0530

    added cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import {connectDB} from "./lib/db.js";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -11,6 +12,10 @@ dotenv.config();
 const PORT= process.env.PORT;
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 
 app.use("/api/auth", authRoutes);
