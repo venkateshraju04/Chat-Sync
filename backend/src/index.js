b0d86ac5a5b7202967ac479ae41194c6603964ab
venkatesh <venkatesh1754673niz@gmail.com>
2025-02-14 20:21:29 +0530

    minor changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,23 +6,27 @@ import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import {app,server} from "./lib/socket.js";
 
-const app=express();
+
 dotenv.config();
 const PORT= process.env.PORT;
 app.use(cookieParser());
-app.use(express.json());
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }));
+
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '3mb', extended: true }));
 
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log("server listening on port:"+PORT);
     connectDB();
 })
