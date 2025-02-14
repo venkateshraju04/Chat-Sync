6e7e07580dab2fb00da1885a4d772727dd856fd3
venkatesh <venkatesh1754673niz@gmail.com>
2025-02-14 20:26:07 +0530

    fixed error in login page navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,8 +13,9 @@ import { Toaster } from 'react-hot-toast';
 import { useThemeStore } from './store/useThemeStore';
 
 const App=()=>{
-  const {authUser, checkAuth, isCheckingAuth}=useAuthStore();
+  const {authUser, checkAuth, isCheckingAuth, onlineUsers}=useAuthStore();
   const {theme}=useThemeStore();
+  console.log({onlineUsers});
   useEffect(()=>{
     checkAuth();
   },[checkAuth]);
@@ -34,7 +35,7 @@ const App=()=>{
     <Routes>
       <Route path="/" element={authUser? <HomePage /> : <Navigate to="/login"/> } />
       <Route path="/signup" element={!authUser ?<SignUpPage />:  <Navigate to="/"/>} />
-      <Route path="/login" element={!authUser ?<LoginPage />:  <Navigate to="/login"/>} />
+      <Route path="/login" element={!authUser ?<LoginPage />:  <Navigate to="/"/>} />
       <Route path="/settings" element={<SettingsPage />} />
       <Route path="/profile" element={authUser? <ProfilePage /> :<Navigate to="/login"/>} />
     </Routes>
