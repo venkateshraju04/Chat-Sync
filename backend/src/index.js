66dd580ac625c7ef36dc357318a715368c1b1f1f
venkatesh <venkatesh1754673niz@gmail.com>
2025-02-15 14:44:35 +0530

    minor changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,13 +14,15 @@ dotenv.config();
 const PORT= process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }));
+
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
